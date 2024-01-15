1b5701392c9cac139003b8b050283b0f3b178906
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-01-15 20:50:25 +0530

    feature(admin/client): Cart Modal added

---
+++ src/components/cartModal/cartModal.js
@@ -0,0 +1,136 @@
+"use client";
+
+import { Fragment, useContext, useEffect } from "react";
+import CommonModel from "../commonModel";
+import { GlobalContext } from "@/context";
+import { deleteFromCart, getAllCartItems } from "@/services/cart/cart";
+import { toast } from "react-toastify";
+export default function CartModal() {
+  const {
+    showCartModal,
+    setShowCartModal,
+    user,
+    cartitems,
+    setCartitems,
+    componentLabelLoader,
+    setcomponentLabelLoader,
+  } = useContext(GlobalContext);
+  async function extractAllCartItems() {
+    const response = await getAllCartItems(user?._id);
+    if (response.success) {
+      setCartitems(response.data);
+      localStorage.setItem("cartItems", JSON.stringify(response.data));
+    }
+  }
+
+  useEffect(() => {
+    if (user !== null) extractAllCartItems();
+  }, [user]);
+
+  const handleDeleteCartItem = async (getCartItemID) => {
+    setcomponentLabelLoader({ loading: true, id: getCartItemID });
+    const response = await deleteFromCart(getCartItemID);
+    if (response.success) {
+      setcomponentLabelLoader({ loading: false, id: "" });
+      toast.success(response.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      extractAllCartItems();
+    } else {
+      setcomponentLabelLoader({ loading: false, id: "" });
+      toast.error(response.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
+  return (
+    <CommonModel
+      showButtons={true}
+      show={showCartModal}
+      setShow={setShowCartModal}
+      mainContent={
+        cartitems && cartitems.length ? (
+          <ul role="list" className="-my-6 divide-y divide-gray-300">
+            {cartitems.map((cartItem) => (
+              <li key={cartItem.id} className="flex py-6">
+                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                  <img
+                    src={
+                      cartItem &&
+                      cartItem.productID &&
+                      cartItem.productID.imageUrl
+                    }
+                    alt="Cart Item"
+                    className="h-full w-full object-cover object-center"
+                  />
+                </div>
+                <div className="ml-4 flex flex-1 flex-col">
+                  <div>
+                    <div className="flex justify-between text-base font-medium text-gray-900">
+                      <h3>
+                        <a>
+                          {cartItem &&
+                            cartItem.productID &&
+                            cartItem.productID.name}
+                        </a>
+                      </h3>
+                    </div>
+                    <p className="mt-1 text-sm text-gray-600">
+                      $
+                      {cartItem &&
+                        cartItem.productID &&
+                        cartItem.productID.price}
+                    </p>
+                  </div>
+                  <div className="flex flex-1 items-end justify-between text-sm">
+                    <button
+                      type="button"
+                      className="font-medium  text-yellow-800 rounded sm:order-2"
+                      onClick={() => handleDeleteCartItem(cartItem._id)}
+                    >
+                      {componentLabelLoader &&
+                      componentLabelLoader.loading &&
+                      componentLabelLoader.id === cartItem._id ? (
+                        <componentLabelLoader
+                          text={"Removing"}
+                          color={"#00000"}
+                          loading={
+                            componentLabelLoader && componentLabelLoader.loading
+                          }
+                        />
+                      ) : (
+                        "Remove"
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : null
+      }
+      buttonComponent={
+        <Fragment>
+          <button
+            type="button"
+            className=" mt-1.5 w-full inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+          >
+            Go To Cart
+          </button>
+          <button
+            disabled={cartitems && cartitems.length === 0}
+            type="button"
+            className=" mt-1.5 w-full inline-block bg-black text-white px-5 py-3 text-xs font-medium uppercase tracking-wide disabled:opacity-50"
+          >
+            Checkout
+          </button>
+          <div className="mt-6 flex justify-center text-sm text-gray-600 ">
+            <button type="button" className=" font-medium text-gray-600">
+              Continue Shopping <span aria-hidden="true"> &rarr;</span>
+            </button>
+          </div>
+        </Fragment>
+      }
+    />
+  );
+}
