94227d1308c93eafa76ec7c456ba29a17abe08cb
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-01-21 09:50:42 +0530

    improve(admin/client): new functionalities add to cart modal

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -3,12 +3,20 @@
 import CommonCart from "@/components/CommonCart/commonCart";
 import DotLoader from "@/components/DotLoader/DotLoader";
 import { GlobalContext } from "@/context";
-import { getAllCartItems } from "@/services/cart/cart";
+import { deleteFromCart, getAllCartItems } from "@/services/cart/cart";
 import { useContext, useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function Cart() {
-  const { user, setCartitems, cartitems, pageLevelLoader, setpageLevelLoader } =
-    useContext(GlobalContext);
+  const {
+    user,
+    setCartitems,
+    cartitems,
+    pageLevelLoader,
+    setpageLevelLoader,
+    componentLabelLoader,
+    setcomponentLabelLoader,
+  } = useContext(GlobalContext);
   async function extractAllCartItems() {
     setpageLevelLoader(true);
     const response = await getAllCartItems(user?._id);
@@ -18,6 +26,23 @@ export default function Cart() {
       localStorage.setItem("cartItems", JSON.stringify(response.data));
     }
   }
+
+  const handleDeleteCartItem = async (getCartItemID) => {
+    setcomponentLabelLoader({ loading: true, id: getCartItemID });
+    const response = await deleteFromCart(getCartItemID);
+    if (response.success) {
+      setcomponentLabelLoader({ loading: false, id: "" });
+      toast.success(response.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      extractAllCartItems();
+    } else {
+      setcomponentLabelLoader({ loading: false, id: "" });
+      toast.error(response.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  };
 
   useEffect(() => {
     if (user !== null) extractAllCartItems();
@@ -30,5 +55,11 @@ export default function Cart() {
       </div>
     );
   }
-  return <CommonCart cartItems={cartitems} />;
+  return (
+    <CommonCart
+      componentLabelLoader={componentLabelLoader}
+      handleDeleteCartItem={handleDeleteCartItem}
+      cartItems={cartitems}
+    />
+  );
 }
