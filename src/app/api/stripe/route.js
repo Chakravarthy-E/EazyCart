5f8f2dbaf57807925b31ce16c687e2347d528c6b
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2024-01-30 11:19:40 +0530

    feature: Stripe method implemented

---
+++ src/app/api/stripe/route.js
@@ -0,0 +1,38 @@
+import AuthUser from "@/middleware/AuthUser";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+const secretkey = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY;
+const stripe = require("stripe")(secretkey);
+
+export async function POST(req) {
+  try {
+    const isAuthUser = await AuthUser(req);
+    if (isAuthUser) {
+      const res = await req.json();
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: res,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout" + "?status=success",
+        cancel_url: "http://localhost:3000/checkout" + "?status=cancel",
+      });
+      return NextResponse.json({
+        success: true,
+        id: session.id,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Your are not authenticated",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      status: 500,
+      success: false,
+      message: "Something went wrong, Please try again",
+    });
+  }
+}
